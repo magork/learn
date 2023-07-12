9c4b92487bb42dc89dbf62b3dad2202cf32f245e
magork <magork87@gmail.com>
2023-07-12 18:04:35 +0300

    start face of SpringBoot

---
+++ src/main/java/com/v/ElsoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.v;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v/ElsoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
