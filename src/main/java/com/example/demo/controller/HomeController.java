7279f5a541ae022022a38a1305bcdaba819937b2
magork <magork87@gmail.com>
2023-07-20 23:47:13 +0300

    Exception handeling

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,10 +9,14 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Story;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -28,6 +32,18 @@ public class HomeController {
         model.addAttribute("stories", getStories());
         
         return "stories";
+    }
+
+    @RequestMapping("/user/{id}")
+    public String searchForUser(@PathVariable(value = "id") String id ) throws Exception {
+        if (id == null)
+            throw new Exception("Nincs ilyen ID-val felhasznalonk!");
+        return "user";
+    }
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandler(HttpServletRequest rA, Exception ex, Model model){
+        model.addAttribute("errMessage", ex.getMessage());
+        return "exceptionHandler";
     }
     
     private ArrayList<Story> getStories(){
